adc6043704cbd6783e5d21fdbe9c8fc41203c49c
aim2bpg <honotaka2016@gmail.com>
2021-10-15 21:18:16 +0900

    Devise導入〜ユーザー機能の拡張前まで

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_message(message, klass)
+    content_tag(:div, class: "alert alert-#{klass}") do
+      concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+      concat raw(message)
+    end
+  end
 end
