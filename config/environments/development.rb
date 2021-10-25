309706e59fe1d02b65147bb16d1819398d1592a5
aim2bpg <honotaka2016@gmail.com>
2021-10-25 17:07:33 +0900

    feedback code review

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     port: 3000
   }
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
