3ed878ba3c1c740f741cc4a042b57f5d74b90dd9
aim2bpg <honotaka2016@gmail.com>
2021-11-08 15:37:09 +0900

    Add testing

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    alice = users(:alice)
+    bob = users(:bob)
+    report = reports(:alices_report)
+
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
+
+  test '#created_on' do
+    report = reports(:alices_report)
+
+    assert Date.parse(report.created_on.to_s)
+    report.created_at = '2021-13-32'
+    assert_raises(NoMethodError) do
+      Date.parse(report.created_on.to_s)
+    end
+  end
 end
